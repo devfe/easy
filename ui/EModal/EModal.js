3fd36c25ae10f60356b2e2cc4b764785888d0ec4
keelii <zhouqili@jd.com>
2015-04-30 16:59:15 +0800

    EModal starting...

--- ui/EModal/EModal.js
+++ ui/EModal/EModal.js
@@ -1,5 +1,5 @@
 /* ------------------------------------------------------------------------
- * @Name: Plugins
+ * @Name: EModal
  * @Author: keelii
  * @Version: 1.0.0
  * ------------------------------------------------------------------------
@@ -8,38 +8,146 @@
 
 (function ($, window, document) {
     'use strict';
-    // 插件名称：新建插件全局替换字符 Plugins 即可
-    var EPluginName = 'Plugins';
+    // 插件名称：新建插件全局替换字符 EModal 即可
+    var EPluginName = 'EModal';
 
     // 插件版本
     var EPluginVersion = '@VERSION';
 
     var emptyFunction = function() {};
 
+    /*
+
+
+
+    */
+
+    var wrap = '\
+        <div id="{id}" class="EModal" data-role="EModal">\
+            <div class="EModal-title">{title}</div>\
+            <div class="EModal-content">{content}</div>\
+        </div>';
+
+
     // 插件参数默认值
     var defaults = {
-        propertyName: 'value'
+        id: null,
+        type: 'html',
+        content: '',
+        hasOverLay: true,
+        template: {
+            title: '提示',
+            wrap: wrap,
+            overLay: '<div data-modal="overlay"></div>',
+            iframe: '<iframe src="{src}" marginwidth="0" marginheight="0" frameborder="0" scrolling="no"></iframe>'
+        },
+        selector: {
+            trigger : '[data-modal="trigger"]',
+            content : '[data-modal="content"]',
+            close   : '[data-modal="close"]',
+            ok      : '[data-modal="ok"]',
+            cancel  : '[data-modal="cancel"]',
+        },
+        onReady: emptyFunction,
+        onClose: emptyFunction,
+        onCancel: emptyFunction,
+        onOk: emptyFunction,
     };
 
-    function Plugins($element, options) {
-        this.$el = $element;
+    function EModal($element, options) {
+        this.$el       = $element;
 
-        this.settings = $.extend(true, {}, defaults, options) ;
+        this.settings  = $.extend(true, {}, defaults, options) ;
 
         this._defaults = defaults;
-        this._name = EPluginName;
+        this._name     = EPluginName;
         this._version  = EPluginVersion;
+        this._guid     = $element.data('Eguid');
 
         this.init();
     }
 
-    Plugins.prototype = {
+    EModal.prototype = {
         init: function() {
+            var selector = this.settings.selector;
+
+            this.$body = $('body');
+
+            this.$trigger = $(selector.trigger);
+
+            this.bindEvent();
 
         },
 
-        pluginMethod: function() {
+        render: function () {
+            var hasOverLay = $('[data-modal="overlay"]').length > 0;
 
+            if ( this.settings.hasOverLay && !hasOverLay ) {
+                this.$overLay.appendTo( this.$body );
+            }
+
+            // 已经存在元素/重复初始化 > 报错
+            if ( $(this.settings.id).length > 0 ) {
+                throw new Error('There is another element called ' + this.settings.id, 'Please change one.');
+            }
+
+            var id      = id || 'EModal-' + this._guid;
+
+            var result  = this.template.wrap.replace('{id}', id);
+            var title   = this.template.title;
+            var content = this.getContent(this.settings.type);
+
+            result = result.replace('{title}', title);
+            result = result.replace('{content}', content);
+
+            this.$body.append(result);
+
+            this.$modal = $(id);
+
+            this.bindEvent(this.$modal);
+        },
+
+        bindEvent: function($ele) {
+            var s = this.settings;
+            var closeSelector = s.selector.close;
+
+            this.$trigger.bind('open', this.handleOpen);
+
+            if ( $ele ) {
+                $ele.unbind()
+                .bind('close', this.close)
+                .bind('ok', this.ok);
+            }
+        },
+
+        handleOpen: function () {
+
+            this.render();
+
+            this.open();
+
+        },
+
+        open: function () {
+
+        },
+
+        close: function () {
+
+        },
+        ok: function () {
+
+        },
+
+        getContent: function () {
+            var s = this.settings;
+            var result = s.content;
+
+            if ( type === 'iframe' ) {
+                result = s.template.iframe.replace('{src}', s.content);
+            }
+
+            return result;
         }
     };
 
@@ -55,7 +163,7 @@
             return this.each(function () {
                 $(this).data('Eguid', $.fn[EPluginName + '_guid']++);
 
-                var EPluginInstance = new Plugins($(this), options);
+                var EPluginInstance = new EModal($(this), options);
 
                 if ( !$(this).data(EPluginName) ) {
                     $(this).data(EPluginName, EPluginInstance);
--- ui/EModal/README.md
+++ ui/EModal/README.md
@@ -1,1 +1,8 @@
 #EModal@1.0.0
+
+1. 模态弹层类型
+    * html（默认）
+    * alert 提醒
+    * confirm 确认，是否
+    * prompt 提示（输入）
+    * iframe 弹出页面
